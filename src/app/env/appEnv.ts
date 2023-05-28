e31b57646d74cc2f85143fdd806636d939db314c
jtspinelli <contato@jonathanspinelli.com>
2023-05-28 18:21:31 -0300

    add addEnv.ts

---
+++ src/app/env/appEnv.ts
@@ -0,0 +1,6 @@
+import 'dotenv/config';
+
+export const appEnv = {
+	port: process.env.PORT,
+	dbUrl: process.env.DB_URL
+};
--- src/main/index.ts
+++ src/main/index.ts
@@ -4,20 +4,18 @@ import cors from 'cors';
 import db from './config/dataSource';
 import { registerRoutes } from './config/httpRoutes.config';
 import { registerMappings } from './config/mappings.config';
+import { appEnv } from '../app/env/appEnv';
 
-const port = process.env.PORT || 8080;
 const app = express();
 app.use(express.json(), cors());
 
-app.get('', (_, res) => {
-	res.send('Hello!');
-});
+app.get('', (_, res) => res.send('Hello!'));
 
 registerRoutes(app);
 registerMappings();
 
 db.initialize().then(() => {
-	app.listen(port, () => {
-		console.log(`App running at port ${port}`);
+	app.listen(appEnv.port, () => {
+		console.log(`App running at port ${appEnv.port}`);
 	});
 });
