c44d5345d4b433896b540b945ddecfb27383e74c
jtspinelli <contato@jonathanspinelli.com>
2023-05-31 19:29:30 -0300

    add migration para criar tabela 'Vagas'

---
+++ src/app/shared/database/migrations/1685571391859-CreateVagasTable.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table, TableColumn } from 'typeorm';
+
+export class CreateVagasTable1685571391859 implements MigrationInterface {
+
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name: 'vagas',
+				columns: [
+					new TableColumn({ name: 'uuid', type: 'varchar', isPrimary: true }),
+					new TableColumn({ name: 'created_at', type: 'varchar' }),
+					new TableColumn({ name: 'recrutador_uuid', type: 'varchar' }),
+					new TableColumn({ name: 'descricao', type: 'varchar' }),
+					new TableColumn({ name: 'nome_empresa', type: 'varchar' }),
+					new TableColumn({ name: 'data_limite', type: 'timestamp' }),
+					new TableColumn({ name: 'ativo', type: 'boolean', default: true }),
+					new TableColumn({ name: 'max_candidatos', type: 'int', isNullable: true })
+				],
+				foreignKeys: [
+					{
+						columnNames: ['recrutador_uuid'],
+						referencedTableName: 'users',
+						referencedColumnNames: ['uuid']
+					}
+				]
+			})
+		);
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable('vagas');
+	}
+
+}
