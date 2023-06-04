88f83528c086d4c52068e01d6f87c39ea3dd408b
jtspinelli <contato@jonathanspinelli.com>
2023-06-04 18:56:32 -0300

    add método 'validateApplyToVaga' aos validators

--- src/app/features/vaga/validators.ts
+++ src/app/features/vaga/validators.ts
@@ -1,7 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response, NextFunction } from 'express';
 import { RecrutadorRequiredError } from '../../shared/exceptions/RecrutadorNotFoundError';
-import { handleControllerError } from '../../shared/exceptions';
+import { handleError } from '../../shared/exceptions';
+import { CandidatoOnlyError } from '../../shared/exceptions/CandidatoOnlyError';
+import { VagaRepository } from './repository';
+import { VagaNotFoundError } from '../../shared/exceptions/VagaNotFoundError';
+import { VagaExpiredError } from '../../shared/exceptions/VagaExpiredError';
 
 export const checkGetVagasQueryParams = (req: Request, res: Response, next: NextFunction) => {
 	if(req.query.limit && (isNaN(Number(req.query.limit)) || Number(req.query.limit) > 10)){
@@ -19,7 +23,6 @@ export const validateCreateVaga = async (req: Request, res: Response, next: Next
 	try {
 		const { authenticatedUser, descricao, nomeEmpresa, maxCandidatos, dataLimite } = req.body;
 		
-		// if(!authenticatedUser.isRecrutador) return res.status(403).send('Apenas Recrutadores podem criar vagas.');
 		if(!authenticatedUser.isRecrutador) throw new RecrutadorRequiredError();
 	
 		if(typeof descricao !== 'string') return res.status(400).send('Propriedade descrição inválida');
@@ -29,6 +32,25 @@ export const validateCreateVaga = async (req: Request, res: Response, next: Next
 		
 		next();		
 	} catch (error: any) {
-		handleControllerError(error, res);
+		handleError(error, res);
 	}
+};
+
+export const validateApplyToVaga = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		if(!req.body.authenticatedUser.isCandidato) throw new CandidatoOnlyError();
+
+		const vagaRepository = new VagaRepository();
+		const vaga = await vagaRepository.get(req.params.vaga_id);
+
+		if(!vaga) throw new VagaNotFoundError();
+
+		if(new Date(vaga.dataLimite) < new Date()) {
+			throw new VagaExpiredError();
+		}
+
+		next();
+	} catch (error:any) {
+		handleError(error, res);
+	}	
 };
