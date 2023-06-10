e6f1d92d18073fe672dd6475eede9a0043e2f55e
jtspinelli <contato@jonathanspinelli.com>
2023-06-10 18:57:37 -0300

    update appEnv com informações do redis db

--- src/app/env/appEnv.ts
+++ src/app/env/appEnv.ts
@@ -4,5 +4,9 @@ export const appEnv = {
 	port: process.env.PORT,
 	dbUrl: process.env.DB_URL,
 	paginationLimit: process.env.PAGINATION_LIMIT,
-	jwtSecret: process.env.JWT_SECRET
+	jwtSecret: process.env.JWT_SECRET,
+	redisPort: Number(process.env.REDIS_PORT),
+	redisHost: process.env.REDIS_HOST,
+	redisUsername: process.env.REDIS_USERNAME,
+	redisPassword: process.env.REDIS_PASSWORD,
 };
